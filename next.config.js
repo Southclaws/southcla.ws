c6b4b25253be91781e723935e7b3739ee9c95151
southclaws <south@cla.ws>
2023-12-09 17:55:27 +0000

    add substack redirects

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
+
+  async redirects() {
+    return [
+      {
+        source: "/p/:slug*",
+        destination: "/:slug*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = withMDX(nextConfig);
