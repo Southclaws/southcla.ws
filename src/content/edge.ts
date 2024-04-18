771db9b944ec97864b1a592b8592afdcf86257f3
Barnaby <accounts@southcla.ws>
2024-04-18 17:55:38 +0100

    use public url var

--- src/content/edge.ts
+++ src/content/edge.ts
@@ -1,6 +1,6 @@
 import type { Metadata } from "./content";
 
-const URL = process.env["VERCEL_URL"] ?? "http://localhost:3000";
+const URL = process.env["NEXT_PUBLIC_VERCEL_URL"] ?? "http://localhost:3000";
 
 export async function getPostMetadata(slug: string): Promise<Metadata> {
   const response = await fetch(`${URL}/meta?slug=${slug}`);
