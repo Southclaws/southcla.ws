2d167ef893a6bb78d322fec8107194218f906b9a
Barnaby <accounts@southcla.ws>
2024-04-18 17:52:33 +0100

    update global title and description meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,9 @@ import "./globals.css";
 import { hedvig, inter, orbiter } from "@/app/fonts";
 
 export const metadata: Metadata = {
-  title: "barney's tech blog | southclaws",
+  title: "barney's tech blog",
   description:
-    "multidisciplinary maker | this site is a never-ending exploration into the details",
+    "I'm a multidisciplinary maker. this site is a never-ending exploration into the details. aka southclaws.",
   metadataBase: new URL("https://southcla.ws"),
 };
 
